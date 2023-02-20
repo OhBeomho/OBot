c0d928276fe13d87f24d7fe5665aa5c4032c3d81
OhBeomho <tyler0322@naver.com>
2023-02-20 19:32:21 +0900

    help command

--- src/slashCommands/commands.ts
+++ src/slashCommands/commands.ts
@@ -1,10 +1,11 @@
 import ping from "./ping"
 import avatar from "./avatar"
 import talkpoints from "./talkpoints"
+import help from "./help"
 import { Collection } from "discord.js"
 import SlashCommandData from "./SlashCommandData"
 
-const commands = [ping, avatar, talkpoints]
+const commands = [ping, avatar, talkpoints, help]
 const collection = new Collection<string, SlashCommandData>()
 
 for (let command of commands)
