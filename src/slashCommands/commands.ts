13741999393b4867f1f7138cdb82d31b7561f96d
OhBeomho <tyler0322@naver.com>
2023-02-24 18:02:11 +0900

    New command

--- src/slashCommands/commands.ts
+++ src/slashCommands/commands.ts
@@ -12,8 +12,9 @@ import addRole from "./role/addRole"
 import deleteRole from "./role/deleteRole"
 import removeRole from "./role/removeRole"
 import roleColor from "./role/roleColor"
+import roleList from "./role/roleList"
 
-const commands = [ping, avatar, talkpoints, list, leaderboard, version, kick, createRole, deleteRole, addRole, removeRole, roleColor]
+const commands = [ping, avatar, talkpoints, list, leaderboard, version, kick, createRole, deleteRole, addRole, removeRole, roleColor, roleList]
 const collection = new Collection<string, SlashCommandData>()
 
 for (let command of commands)
---
+++ src/slashCommands/role/roleList.ts
@@ -0,0 +1,32 @@
+import { CommandInteraction, SlashCommandBuilder, EmbedBuilder, GuildScheduledEvent } from "discord.js"
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("role-list")
+    .setDescription("해당 서버의 역할 목록을 보여줍니다."),
+  async execute(interaction: CommandInteraction) {
+    const guildName = interaction.guild?.name
+    const roles = interaction.guild?.roles
+    const roleList = new EmbedBuilder().setColor("Blue").setTitle(guildName + " 서버 역할 목록")
+
+    if (!guildName && !roles) {
+      await interaction.reply("서버 이름과 역할을 가져올 수 없습니다.")
+      return
+    } else if (!roles) {
+      roleList.setDescription("역할이 없습니다.")
+      await interaction.reply({ embeds: [roleList] })
+      return
+    }
+
+    let roleText = ""
+    for (let role of roles.cache.values()) {
+      if (role.tags?.botId) continue
+
+      roleText += `${role}\n`
+    }
+
+    roleList.setDescription(roleText)
+
+    await interaction.reply({ embeds: [roleList] })
+  }
+}
